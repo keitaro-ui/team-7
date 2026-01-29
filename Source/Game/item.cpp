2ee04c01f8ec248d2ad673446279b79d15ddd4d7
keitaro-ui <kei.kei.taro305@gmail.com>
2026-01-29 14:30:04 +0900

    item改善

---
+++ Source/Game/item.cpp
@@ -0,0 +1,59 @@
+#include "item.h"
+
+Item::Item()
+{
+	model = std::make_unique<Model>("Data/Model/Boad/Box.mdl");
+}
+
+Item::~Item()
+{
+}
+
+void Item::Update(float elapsedTime)
+{
+}
+
+void Item::Render(const RenderContext& rc, ModelRenderer* renderer)
+{
+	renderer->Render(rc, transform, model.get(), ShaderId::Lambert);
+}
+
+void Item::Spawn()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int>distX(0, grid.GRID_MAX - 1);
+	std::uniform_int_distribution<int>distY(0, grid.GRID_MAX - 1);
+	while (true)
+	{
+		int x = distX(gen);
+		int	y = distY(gen);
+
+		if (grid.map[y][x] == 0)
+		{
+			int playerX = PlayerManager::Instance().GetPlayer()->GetPlayerX();
+			int playerY = PlayerManager::Instance().GetPlayer()->GetPlayerY();
+
+			if (playerY != y && playerX != x)
+			{
+				
+				break;
+			}
+		}
+	}
+}
+
+void Item::deleteSmallNumber()
+{
+	for (int x = 0; x < grid.GRID_MAX; x++)
+	{
+		for (int y = 1; y < grid.GRID_MAX; y++)
+		{
+			//8ˆÈ‰º‚È‚çÁ‚·
+			if (grid.map[y][x] < 4)
+			{
+				grid.map[y][x] = 0;
+			}
+		}
+	}
+}
--- Source/Game/item.h
+++ Source/Game/item.h
@@ -1,6 +1,29 @@
 #pragma once
-class item
+
+#include "Enemy.h"
+#include <random>
+#include "Grid.h"
+#include "PlayerManager.h"
+
+class Item : public Enemy
 {
 public:
+	Item();
+	~Item() override;
 
+	//XVˆ—
+	void Update(float elapsedTime) override;
+
+	//•`‰æˆ—
+	void Render(const RenderContext& rc, ModelRenderer* renderer) override;
+
+	//oŒ»
+	void Spawn();
+
+	//­‚È‚¢”‚Ìbox‚ğÁ‚·
+	void deleteSmallNumber();
+
+private:
+	Grid grid;
+	std::unique_ptr<Model> model = nullptr;
 };
