8c71b1d5131c255d1a8074eeb40167969ea4176a
keitaro-ui <kei.kei.taro305@gmail.com>
2026-02-01 05:14:07 +0900

    背景を灰色にしました

--- Source/Scene/SceneGame.cpp
+++ Source/Scene/SceneGame.cpp
@@ -191,11 +191,6 @@ void SceneGame::Render()
 	rc.lightDirection = { 0.0f, -1.0f, 0.0f };	// ライト方向（下方向）
 	rc.renderState = graphics.GetRenderState();
 
-	//描画処理
-	//RenderContext rc;
-	rc.renderState = graphics.GetRenderState();
-	rc.lightDirection = { 0.0f , -1.0f , 0.0f };	//ライト方向（下方向）
-
 	//カメラパラメータ設定
 	Camera& camera = Camera::Instance();
 	rc.view = camera.GetView();
